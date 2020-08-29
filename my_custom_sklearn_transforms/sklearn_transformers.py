c3ecf4b21d05f7d2ee5643a3be80909ec74f687d
Carla Santos <7880691+julciane@users.noreply.github.com>
2020-08-29 18:01:19 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,26 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class CustomColumnsImputer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        
+        # Segundo preenchemos as informações de ingles com zero
+        data['INGLES'].fillna(0, inplace=True)
+                
+        for row in data.loc[data['NOTA_GO'].isnull()].itertuples():
+                if row.REPROVACOES_GO == 0:
+                    data.at[row.Index, 'NOTA_GO'] = data['NOTA_GO'].mean()
+                else:
+                    data.at[row.Index, 'NOTA_GO'] = 0
+                
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data   
